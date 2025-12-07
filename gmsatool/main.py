96a25524e463a512a547d531fb37acbf483f8036
int3x <niket5151@gmail.com>
2025-12-06 20:17:29 -0500

    Switched from typer to cyclopts; reduced redundancy

--- gmsatool/main.py
+++ gmsatool/main.py
@@ -1,8 +1,9 @@
-import typer
 import logging
-import traceback
+from typing import Annotated, Optional
 
-from typing_extensions import Annotated
+import cyclopts
+from cyclopts import Parameter
+
 from gmsatool.commands.enum.find_gmsa import GMSAEnumerator
 from gmsatool.commands.gmsa.read_password import GMSAReader
 from gmsatool.commands.gmsa.access import GMSAMembership
@@ -11,125 +12,121 @@ from gmsatool.protocols.ldap import get_ldap_session
 from gmsatool.helpers.common import logger, bcolors
 
 
-app = typer.Typer(context_settings={"help_option_names": ["-h", "--help"]}, add_completion=False)
-enum_app = typer.Typer()
-gmsa_app = typer.Typer()
+Domain = Annotated[str, Parameter(name=["--domain", "-d"], help="The domain name")]
+DC = Annotated[Optional[str], Parameter(name="--dc", help="The target domain controller (IP or FQDN). If omitted, defaults to the domain FQDN")]
+Username = Annotated[Optional[str], Parameter(name=["--username", "-u"], help="The username")]
+Password = Annotated[Optional[str], Parameter(name=["--password", "-p"], help="The password")]
+Hash = Annotated[Optional[str], Parameter(name=["--hash", "-H"], help="The NT hash for the domain account")]
+Kerberos = Annotated[bool, Parameter(name="-k", help="Use Kerberos authentication. Default TGT location is /tmp/krb5cc_$(id -u)")]
+LDAPS = Annotated[bool, Parameter(name="--ldaps", help="Use LDAPS (port 636)", negative="")]
+Verbose = Annotated[bool, Parameter(name=["--verbose", "-v"], help="Enable verbose output", negative="")]
 
-app.add_typer(enum_app, name="enum", help="Subcommands for enumeration")
-app.add_typer(gmsa_app, name="gmsa", help="Subcommands for reading gMSA password and access manipulation")
+
+app = cyclopts.App()
+enum_app = cyclopts.App(name="enum", help="Enumeration Subcommands")
+gmsa_app = cyclopts.App(name="gmsa", help="gMSA Subcommands")
+
+app.command(enum_app)
+app.command(gmsa_app)
 
 
-def set_verbosity(value):
-    if value:
+def validate_auth_params(username, password, hash):
+    """Validate authentication parameter combinations"""
+    if password is not None and hash is not None:
+        raise ValueError("Cannot use both --password and --hash simultaneously")
+
+    if username is not None and password is None and hash is None:
+        raise ValueError("When --username is specified, either --password or --hash must be provided")
+
+
+def setup_and_connect(domain, dc, username, password, hash, kerberos, ldaps, verbose):
+    """Setup logging, process args, and return LDAP session"""
+    if verbose:
         logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.INFO)
 
+    if password is None and hash is not None:
+        password = "0" * 32 + ":" + hash
 
-@enum_app.command(help="Find gMSA accounts, users with password read access, and users who can modify that access")
+    dc = dc if dc else domain
+
+    return get_ldap_session(domain, dc, ldaps, username, password, kerberos, all_info=True)
+
+
+@enum_app.command
 def find_gmsa(
-    domain: Annotated[str, typer.Option("--domain", "-d", help="The domain FQDN")],
-    target: Annotated[str, typer.Option("--target", help="The target gMSA account (opional)")] = None,
-    dc: Annotated[str, typer.Option("--dc", help="The target domain controller (IP or FQDN). If omitted, defaults to the domain FQDN")] = None,
-    username: Annotated[str, typer.Option("--username", "-u", help="The username")] = None,
-    password: Annotated[str, typer.Option("--password", "-p", help="The password")] = None,
-    hash: Annotated[str, typer.Option("--hash", "-H", help="The NT hash for the domain account")] = None,
-    kerberos: Annotated[bool, typer.Option("-k", help="Use Kerberos authentication. Default TGT location is /tmp/krb5cc_`id -u`")] = False,
-    ldaps: Annotated[bool, typer.Option("--ldaps", help="Use LDAPS (port 636)")] = False,
-    verbose: Annotated[bool, typer.Option("--verbose", "-v", help="Enable verbose output", callback=set_verbosity)] = False,
+    domain: Domain,
+    dc: DC = None,
+    username: Username = None,
+    password: Password = None,
+    hash: Hash = None,
+    kerberos: Kerberos = False,
+    ldaps: LDAPS = False,
+    verbose: Verbose = False,
+    target: Annotated[Optional[str], Parameter(name="--target", help="The target gMSA account (optional)")] = None,
 ):
-    try:
-        if username is not None and (password is None and hash is None):
-            logger.error(f"{bcolors.FAIL}[!] When providing a username, please also provide either the cleartext password or the NT hash{bcolors.ENDC}")
-            return
-        if password is None and hash is not None:
-            password = "0" * 32 + ":" + hash
-        if dc is None:
-            dc = domain
-
-        ldap_session = get_ldap_session(domain, dc, ldaps, username, password, kerberos, all_info=True)
-        gmsa_enumerator = GMSAEnumerator(domain, target, ldap_session)
-        gmsa_enumerator.get_gmsa_accounts()
-
-    except Exception as e:
-        logger.error(f"{bcolors.FAIL}[!] Error encountered while running enum command{bcolors.ENDC}")
-        traceback.print_exc()
+    """Find gMSA accounts, users with password read access, and users who can modify that access"""
+    validate_auth_params(username, password, hash)
+    ldap_session = setup_and_connect(domain, dc, username, password, hash, kerberos, ldaps, verbose)
+    gmsa_enumerator = GMSAEnumerator(domain, target, ldap_session)
+    gmsa_enumerator.get_gmsa_accounts()
 
 
-@gmsa_app.command(help="Read gMSA password of a target account")
+@gmsa_app.command
 def read_password(
-    domain: Annotated[str, typer.Option("--domain", "-d", help="The domain name")],
-    target: Annotated[str, typer.Option("--target", help="The target gMSA account")],
-    dc: Annotated[str, typer.Option("--dc", help="The target domain controller (IP or FQDN). If omitted, defaults to the domain FQDN")] = None,
-    username: Annotated[str, typer.Option("--username", "-u", help="The username")] = None,
-    password: Annotated[str, typer.Option("--password", "-p", help="The password")] = None,
-    hash: Annotated[str, typer.Option("--hash", "-H", help="The NT hash for the domain account")] = None,
-    kerberos: Annotated[bool, typer.Option("-k", help="Use Kerberos authentication. Default TGT location is /tmp/krb5cc_`id -u`")] = False,
-    ldaps: Annotated[bool, typer.Option("--ldaps", help="Use LDAPS (port 636)")] = False,
-    verbose: Annotated[int, typer.Option("--verbose", "-v", help="Enable verbose output", callback=set_verbosity, count=True)] = 0,
+    domain: Domain,
+    target: Annotated[str, Parameter(name="--target", help="The target gMSA account")],
+    dc: DC = None,
+    username: Username = None,
+    password: Password = None,
+    hash: Hash = None,
+    kerberos: Kerberos = False,
+    ldaps: LDAPS = False,
+    verbose: Verbose = False,
 ):
-    try:
-        if username is not None and (password is None and hash is None):
-            logger.error(f"{bcolors.FAIL}[!] When providing a username, please also provide either the cleartext password or the NT hash{bcolors.ENDC}")
-            return
-        if password is None and hash is not None:
-            password = "0" * 32 + ":" + hash
-        if dc is None:
-            dc = domain
-
-        ldap_session = get_ldap_session(domain, dc, ldaps, username, password, kerberos, all_info=True)
-        gmsa_reader = GMSAReader(domain, target, ldap_session)
-        gmsa_reader.read_gmsa_password()
-
-    except Exception as e:
-        logger.error(f"{bcolors.FAIL}[!] Error encountered while running the command{bcolors.ENDC}")
-        traceback.print_exc()
+    """Read gMSA password of a target account"""
+    validate_auth_params(username, password, hash)
+    ldap_session = setup_and_connect(domain, dc, username, password, hash, kerberos, ldaps, verbose)
+    gmsa_reader = GMSAReader(domain, target, ldap_session)
+    gmsa_reader.read_gmsa_password()
 
 
-@gmsa_app.command(help="Grant or deny gMSA password read privilege to a principal")
+@gmsa_app.command
 def access(
-    domain: Annotated[str, typer.Option("--domain", "-d", help="The domain name")],
-    target: Annotated[str, typer.Option("--target", help="The target gMSA account")],
-    principal: Annotated[str, typer.Option("--principal", help="The account designated for privilege modification")],
-    dc: Annotated[str, typer.Option("--dc", help="The target domain controller (IP or FQDN). If omitted, defaults to the domain FQDN")] = None,
-    username: Annotated[str, typer.Option("--username", "-u", help="The username")] = None,
-    password: Annotated[str, typer.Option("--password", "-p", help="The password")] = None,
-    hash: Annotated[str, typer.Option("--hash", "-H", help="The NT hash for the domain account")] = None,
-    kerberos: Annotated[bool, typer.Option("-k", help="Use Kerberos authentication. Default TGT location is /tmp/krb5cc_`id -u`")] = False,
-    ldaps: Annotated[bool, typer.Option("--ldaps", help="Use LDAPS (port 636)")] = False,
-    verbose: Annotated[int, typer.Option("--verbose", "-v", help="Enable verbose output", callback=set_verbosity, count=True)] = 0,
+    domain: Domain,
+    target: Annotated[str, Parameter(name="--target", help="The target gMSA account")],
+    principal: Annotated[str, Parameter(name="--principal", help="The account designated for privilege modification")],
+    dc: DC = None,
+    username: Username = None,
+    password: Password = None,
+    hash: Hash = None,
+    kerberos: Kerberos = False,
+    ldaps: LDAPS = False,
+    verbose: Verbose = False,
 ):
-    try:
-        if username is not None and (password is None and hash is None):
-            logger.error(f"{bcolors.FAIL}[!] When providing a username, please also provide either the cleartext password or the NT hash{bcolors.ENDC}")
-            return
-        if password is None and hash is not None:
-            password = "0" * 32 + ":" + hash
-        if dc is None:
-            dc = domain
-
-        logger.debug(f"{bcolors.WARNING}\n[*] The feature to remove gMSA password read access has not been implemented!{bcolors.ENDC}")
-
-        ldap_session = get_ldap_session(domain, dc, ldaps, username, password, kerberos, all_info=True)
-        gmsa_enumerator = GMSAMembership(domain, target, principal, ldap_session)
-        gmsa_enumerator.add_readgmsapassword_access()
-
-    except Exception as e:
-        logger.error(f"{bcolors.FAIL}[!] Error encountered while running the command{bcolors.ENDC}")
-        traceback.print_exc()
+    """Grant or deny gMSA password read privilege to a principal"""
+    logger.debug(f"{bcolors.WARNING}\n[*] The feature to remove gMSA password read access has not been implemented!{bcolors.ENDC}")
+    
+    validate_auth_params(username, password, hash)
+    ldap_session = setup_and_connect(domain, dc, username, password, hash, kerberos, ldaps, verbose)
+    gmsa_enumerator = GMSAMembership(domain, target, principal, ldap_session)
+    gmsa_enumerator.add_readgmsapassword_access()
 
 
-@gmsa_app.command(help="Automated enumeration and abuse of gMSA privileges (experimental)")
+@gmsa_app.command
 def auto(
-    domain: Annotated[str, typer.Option("--domain", "-d", help="The domain name")],
-    dc: Annotated[str, typer.Option("--dc", help="The target domain controller (IP or FQDN). If omitted, defaults to the domain FQDN")] = None,
-    username: Annotated[str, typer.Option("--username", "-u", help="The username")] = None,
-    password: Annotated[str, typer.Option("--password", "-p", help="The password")] = None,
-    hash: Annotated[str, typer.Option("--hash", "-H", help="The NT hash for the domain account")] = None,
-    kerberos: Annotated[bool, typer.Option("-k", help="Use Kerberos authentication. Default TGT location is /tmp/krb5cc_`id -u`")] = False,
-    ldaps: Annotated[bool, typer.Option("--ldaps", help="Use LDAPS (port 636)")] = False,
-    verbose: Annotated[int, typer.Option("--verbose", "-v", help="Enable verbose output", callback=set_verbosity, count=True)] = 0,
+    domain: Domain,
+    dc: DC = None,
+    username: Username = None,
+    password: Password = None,
+    hash: Hash = None,
+    kerberos: Kerberos = False,
+    ldaps: LDAPS = False,
+    verbose: Verbose = False,
 ):
+    """Automated enumeration and abuse of gMSA privileges (experimental)"""
+    validate_auth_params(username, password, hash)
     logger.error(f"{bcolors.FAIL}[!] Feature not implemented!{bcolors.ENDC}")
 
 
