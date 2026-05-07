a71595c5f10d4aee44cff1ecff2b83c084a76918
Opcode <niket5151@gmail.com>
2026-05-07 18:07:41 -0400

    - Implemented extensive resolution of security descriptor on gMSA account to capture all variants which allow us to modify who can read the password (e.g. GenericAll, GenericWrite, WriteProperty over msDS-GroupMSAMembership attribute)
    - Also accounted for well-known SIDs in sid_to_samaccountname
    
    - In case of empty msDS-GroupMSAMembership, it displays Domain Admins has read privilege

--- gmsatool/commands/enum/find_gmsa.py
+++ gmsatool/commands/enum/find_gmsa.py
@@ -1,11 +1,43 @@
 from ldap3.protocol.microsoft import security_descriptor_control
-from winacl.dtyp.security_descriptor import SECURITY_DESCRIPTOR
 from rich import box
 from rich.console import Console
 from rich.table import Table
+from winacl.dtyp.ace import ACCESS_MASK, ADS_ACCESS_MASK, STANDARD_ACCESS_MASK, ACEType
+from winacl.dtyp.security_descriptor import SECURITY_DESCRIPTOR
+from winacl.dtyp.sid import well_known_sids_sid_name_map
 
-from gmsatool.protocols.ldap import get_entry, sid_to_samaccountname, LDAPNoResultsError
-from gmsatool.helpers.common import logger, bcolors
+from gmsatool.helpers.common import bcolors, logger
+from gmsatool.protocols.ldap import LDAPNoResultsError, get_entry, sid_to_samaccountname
+
+# https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-ada2/c651f64d-5e92-4d12-9011-e6811ed306aa
+GROUP_MSA_MEMBERSHIP_GUID = "888eedd6-ce04-df40-b462-b8a50e41ba38"
+
+DENY_ACE_TYPES = {
+    ACEType.ACCESS_DENIED_ACE_TYPE,
+    ACEType.ACCESS_DENIED_OBJECT_ACE_TYPE,
+    ACEType.ACCESS_DENIED_CALLBACK_ACE_TYPE,
+    ACEType.ACCESS_DENIED_CALLBACK_OBJECT_ACE_TYPE,
+}
+
+
+def ace_grants_gmsa_write(ace):
+    if ace.AceType in DENY_ACE_TYPES:
+        return False
+
+    mask = ace.Mask
+
+    if mask & (ACCESS_MASK.GENERIC_ALL | ACCESS_MASK.GENERIC_WRITE):
+        return True
+
+    if mask & (ADS_ACCESS_MASK.GENERIC_ALL | ADS_ACCESS_MASK.GENERIC_WRITE | ADS_ACCESS_MASK.WRITE_DACL | ADS_ACCESS_MASK.WRITE_OWNER):
+        return True
+
+    if mask & ADS_ACCESS_MASK.WRITE_PROP:
+        object_type = str(getattr(ace, "ObjectType", None))
+        if object_type == GROUP_MSA_MEMBERSHIP_GUID or not hasattr(ace, "ObjectType"):
+            return True
+
+    return False
 
 
 class GMSAEnumerator:
@@ -17,7 +49,13 @@ class GMSAEnumerator:
     def get_gmsa_accounts(self):
         try:
             attributes = ["sAMAccountName", "msDS-GroupMSAMembership", "nTSecurityDescriptor"]
-            results = get_entry(self.ldap_session, self.dn, search_filter="(objectClass=msDS-GroupManagedServiceAccount)", attributes=attributes, controls=security_descriptor_control(sdflags=0x07))
+            results = get_entry(
+                self.ldap_session,
+                self.dn,
+                search_filter="(objectClass=msDS-GroupManagedServiceAccount)",
+                attributes=attributes,
+                controls=security_descriptor_control(sdflags=0x07),
+            )
         except LDAPNoResultsError as e:
             logger.error(f"{bcolors.FAIL}[!] {e}{bcolors.ENDC}")
             return None, None
@@ -29,15 +67,43 @@ class GMSAEnumerator:
             if result["attributes"]["msDS-GroupMSAMembership"]:
                 gmsa_sd = SECURITY_DESCRIPTOR.from_bytes(result["attributes"]["msDS-GroupMSAMembership"])
                 for ace in gmsa_sd.Dacl.aces:
+                    sid_str = str(ace.Sid)
+                    if sid_str in well_known_sids_sid_name_map:
+                        continue
                     principal, principal_dn, principal_type = sid_to_samaccountname(self.ldap_session, self.dn, ace.Sid)
-                    read_privileges.append({"gmsa": result["attributes"]["sAMAccountName"], "principal": principal, "principal_dn": principal_dn, "principal_type": principal_type})
+                    read_privileges.append(
+                        {
+                            "gmsa": result["attributes"]["sAMAccountName"],
+                            "principal": principal,
+                            "principal_dn": principal_dn,
+                            "principal_type": principal_type,
+                        }
+                    )
+            else:
+                read_privileges.append(
+                    {
+                        "gmsa": result["attributes"]["sAMAccountName"],
+                        "principal": "Domain Admins",
+                        "principal_dn": f"CN=Domain Admins,CN=Users,{self.dn}",
+                        "principal_type": "group",
+                    }
+                )
 
             sd = SECURITY_DESCRIPTOR.from_bytes(result["attributes"]["nTSecurityDescriptor"])
             for ace in sd.Dacl.aces:
-                # https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-ada2/c651f64d-5e92-4d12-9011-e6811ed306aa
-                if hasattr(ace, "ObjectType") and str(ace.ObjectType) == "888eedd6-ce04-df40-b462-b8a50e41ba38":
+                sid_str = str(ace.Sid)
+                if sid_str in well_known_sids_sid_name_map:
+                    continue
+                if ace_grants_gmsa_write(ace):
                     principal, principal_dn, principal_type = sid_to_samaccountname(self.ldap_session, self.dn, ace.Sid)
-                    modify_privileges.append({"gmsa": result["attributes"]["sAMAccountName"], "principal": principal, "principal_dn": principal_dn, "principal_type": principal_type})
+                    modify_privileges.append(
+                        {
+                            "gmsa": result["attributes"]["sAMAccountName"],
+                            "principal": principal,
+                            "principal_dn": principal_dn,
+                            "principal_type": principal_type,
+                        }
+                    )
 
         return read_privileges, modify_privileges
 
@@ -51,13 +117,15 @@ class GMSAEnumerator:
             gmsa_read_principals.add_column("[bold bright_cyan]Principal Type[/bold bright_cyan]")
 
             for entry in read_privileges:
-                principal_type = "Group" if "group" in entry["principal_type"] else "User"
+                principal_type = "Group" if any(x in entry["principal_type"] for x in ("group", "well-known")) else "User"
                 gmsa_read_principals.add_row(entry["gmsa"], entry["principal"], principal_type)
 
             console.print(gmsa_read_principals)
 
         if modify_privileges:
-            gmsa_modify_principals = Table(box=box.ROUNDED, title="\n[bold bright_yellow]Modify privileges[/bold bright_yellow]", title_justify="left")
+            gmsa_modify_principals = Table(
+                box=box.ROUNDED, title="\n[bold bright_yellow]Modify privileges[/bold bright_yellow]", title_justify="left"
+            )
             gmsa_modify_principals.add_column("[bold bright_cyan]gMSA account[/bold bright_cyan]")
             gmsa_modify_principals.add_column("[bold bright_cyan]Access Manager[/bold bright_cyan]")
             gmsa_modify_principals.add_column("[bold bright_cyan]Principal Type[/bold bright_cyan]")
--- gmsatool/protocols/ldap.py
+++ gmsatool/protocols/ldap.py
@@ -1,4 +1,5 @@
-from ldap3 import Server, Connection, SASL, NTLM, KERBEROS, MODIFY_REPLACE, ALL_ATTRIBUTES, SCHEMA, ALL, TLS_CHANNEL_BINDING, ENCRYPT
+from ldap3 import ALL, ALL_ATTRIBUTES, ENCRYPT, KERBEROS, MODIFY_REPLACE, NTLM, SASL, SCHEMA, TLS_CHANNEL_BINDING, Connection, Server
+from winacl.dtyp.sid import well_known_sids_sid_name_map
 
 from gmsatool.helpers.common import logger
 
@@ -16,24 +17,44 @@ def get_ldap_session(domain, dc, ldaps, username, password, kerberos=False, all_
     if kerberos is False:
         if ldaps is True:
             logger.debug(f"[*] Authenticating to ldaps://{dc}:636 with NTLM")
-            ldap_session = Connection(server, user=f"{domain}\\{username}", password=password, authentication=NTLM, auto_bind=True, raise_exceptions=True, channel_binding=TLS_CHANNEL_BINDING)
+            ldap_session = Connection(
+                server,
+                user=f"{domain}\\{username}",
+                password=password,
+                authentication=NTLM,
+                auto_bind=True,
+                raise_exceptions=True,
+                channel_binding=TLS_CHANNEL_BINDING,
+            )
         else:
             logger.debug(f"[*] Authenticating to ldap://{dc}:389 with NTLM")
-            ldap_session = Connection(server, user=f"{domain}\\{username}", password=password, authentication=NTLM, auto_bind=True, raise_exceptions=True, session_security=ENCRYPT)
+            ldap_session = Connection(
+                server,
+                user=f"{domain}\\{username}",
+                password=password,
+                authentication=NTLM,
+                auto_bind=True,
+                raise_exceptions=True,
+                session_security=ENCRYPT,
+            )
     else:
         if ldaps is True:
             logger.debug(f"[*] Authenticating to ldaps://{dc}:636 with Kerberos")
             ldap_session = Connection(server, authentication=SASL, sasl_mechanism=KERBEROS, auto_bind=True, raise_exceptions=True)
         else:
             logger.debug(f"[*] Authenticating to ldap://{dc}:389 with Kerberos")
-            ldap_session = Connection(server, authentication=SASL, sasl_mechanism=KERBEROS, auto_bind=True, raise_exceptions=True, session_security=ENCRYPT)
+            ldap_session = Connection(
+                server, authentication=SASL, sasl_mechanism=KERBEROS, auto_bind=True, raise_exceptions=True, session_security=ENCRYPT
+            )
     return ldap_session
 
 
 def get_entry(ldap_session, dn, search_filter="(objectClass=*)", attributes=ALL_ATTRIBUTES, get_operational_attributes=False, controls=None):
     entries = []
     logger.debug(f"[*] Querying {attributes} attribute on {dn} with search filter {search_filter}")
-    ldap_session.search(search_base=dn, search_filter=search_filter, attributes=attributes, get_operational_attributes=get_operational_attributes, controls=controls)
+    ldap_session.search(
+        search_base=dn, search_filter=search_filter, attributes=attributes, get_operational_attributes=get_operational_attributes, controls=controls
+    )
 
     for item in ldap_session.response:
         if item["type"] == "searchResEntry":
@@ -50,9 +71,13 @@ def modify_attribute(ldap_session, dn, attribute, new_value):
 
 
 def sid_to_samaccountname(ldap_session, dn, sid):
+    sid_str = str(sid)
+    if sid_str in well_known_sids_sid_name_map:
+        name = well_known_sids_sid_name_map[sid_str]
+        return name, "", "well-known"
+
     logger.debug(f"[*] Querying sAMAccountName attribute on {dn} with search filter (objectSid={sid})")
     ldap_session.search(search_base=dn, search_filter=f"(objectSid={sid})", attributes=["sAMAccountName", "objectClass"], size_limit=1)
-
     entries = []
     for item in ldap_session.response:
         if item["type"] == "searchResEntry":
@@ -77,7 +102,11 @@ def samaccountname_to_sid(ldap_session, dn, samaccountname):
 
 def check_group_membership(ldap_session, dn, username, group_dn):
     logger.debug(f"[*] Querying memberOf attribute on {dn} with search filter (sAMAccountName={username})")
-    ldap_session.search(search_base=dn, search_filter=f"(&(sAMAccountName={username})(memberOf:1.2.840.113556.1.4.1941:={group_dn}))", attributes=["distinguishedName"])
+    ldap_session.search(
+        search_base=dn,
+        search_filter=f"(&(sAMAccountName={username})(memberOf:1.2.840.113556.1.4.1941:={group_dn}))",
+        attributes=["distinguishedName"],
+    )
 
     entries = []
     for item in ldap_session.response:
